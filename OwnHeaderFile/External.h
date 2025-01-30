066394c6e7b50676905af4e93155a03e39157519
Raghavan S <147412730+raghavan-s123@users.noreply.github.com>
2025-01-30 15:06:17 +0530

    Add files via upload

---
+++ OwnHeaderFile/External.h
@@ -0,0 +1,11 @@
+class input{
+    private:
+        int sum;
+
+    public:
+        int add(int a, int b){
+            sum = a + b;
+            return sum;
+        }
+};
+
---
+++ OwnHeaderFile/Hello.cpp
@@ -0,0 +1,14 @@
+#include "External.h"
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int a, int b, int c){
+    int sum = a + b + c;
+    return sum;
+}
+int main(){
+    int a = 6;
+    int b = 9;
+    input in;
+    cout << in.add(a, b);
+}
