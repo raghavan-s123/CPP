6aec4e251a52937f1716cb8379f8bd582f7aa9cd
Raghavan S <147412730+raghavan-s123@users.noreply.github.com>
2025-02-04 12:18:38 +0530

    Add files via upload

---
+++ Class/Classes.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Animal {
+    private:
+        int n;
+    public:
+        Animal();
+};
+Animal::Animal(){
+    cout << "HELLO";
+}
+int main(){
+    Animal obj;
+}
